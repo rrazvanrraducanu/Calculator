eefbebbd49c0ef2c76b2b5ed6e712d7c4e3970d9
Razvan <Razvan@DESKTOP-C7RB25H>
2019-05-05 19:28:23 +0300

    Add project files.

---
+++ Calculator/CalculatorDlg.cpp
@@ -0,0 +1,194 @@
+
+// CalculatorDlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Calculator.h"
+#include "CalculatorDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCalculatorDlg dialog
+
+
+
+CCalculatorDlg::CCalculatorDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CALCULATOR_DIALOG, pParent)
+	, m_edit(0)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CCalculatorDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_edit);
+}
+
+BEGIN_MESSAGE_MAP(CCalculatorDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CCalculatorDlg::OnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CCalculatorDlg::OnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON3, &CCalculatorDlg::OnClickedButton3)
+	ON_BN_CLICKED(IDC_BUTTON4, &CCalculatorDlg::OnClickedButton4)
+END_MESSAGE_MAP()
+
+
+// CCalculatorDlg message handlers
+
+BOOL CCalculatorDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+	m_res = 0;
+	m_edit = 0;
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CCalculatorDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CCalculatorDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CCalculatorDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CCalculatorDlg::OnClickedButton1()
+{
+	// TODO: Add your control notification handler code here
+	m_edit = m_edit * 10 + 1;
+	UpdateData(FALSE);
+}
+
+
+void CCalculatorDlg::OnClickedButton2()
+{
+	// TODO: Add your control notification handler code here
+	m_res = m_res + m_edit;
+	m_edit = 0;
+}
+
+
+void CCalculatorDlg::OnClickedButton3()
+{
+	// TODO: Add your control notification handler code here
+	m_edit = m_edit * 10 + 2;
+	UpdateData(FALSE);
+}
+
+
+void CCalculatorDlg::OnClickedButton4()
+{
+	// TODO: Add your control notification handler code here
+	m_edit = m_edit + m_res;
+	UpdateData(FALSE);
+}
---
+++ Calculator/CalculatorDlg.h
@@ -0,0 +1,41 @@
+
+// CalculatorDlg.h : header file
+//
+
+#pragma once
+
+
+// CCalculatorDlg dialog
+class CCalculatorDlg : public CDialogEx
+{
+// Construction
+public:
+	CCalculatorDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_edit;
+	int m_res;
+	afx_msg void OnClickedButton1();
+	afx_msg void OnClickedButton2();
+	afx_msg void OnClickedButton3();
+	afx_msg void OnClickedButton4();
+};
